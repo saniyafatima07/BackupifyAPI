5f672050ab77647246548ace2f15c7c08fe8286b
saniyafatima07 <saniyafatima200631@gmail.com>
2025-05-03 16:54:44 +0530

    Push initial setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = 3000;
+
+
+app.get('/',(req,res) => {
+    res.send("Server running, Yay!!")
+})
+
+app.listen(port, () => {
+    console.log(`App running on http://localhost:${port}`)
+})
