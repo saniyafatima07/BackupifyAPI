41eb6daf678b58a6735757d40145cdad22c9e5b4
saniyafatima07 <saniyafatima200631@gmail.com>
2025-05-04 01:51:35 +0530

    Push scraper

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const path = require('path')
+const scrape = require('./scraper/puppeter')
 
 const app = express()
 const port = 3000;
 
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     res.send("Server running, Yay!!")
 })
+
+app.get('/scrape', async (req, res) => {
+    const url = "https://www.allrecipes.com/article/cooks-to-follow-shaurya-from-india/";
+    const data = await scrape(url);
+
+    if (data) {
+        res.send(data);
+    }
+
+    else {
+        res.status(500).json({ error: "Scraping failed" });
+    }
+});
 
 app.listen(port, () => {
     console.log(`App running on http://localhost:${port}`)
